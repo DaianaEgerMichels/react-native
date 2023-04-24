450936067849682214220f475fce8a4a9e0a9ea0
Daiana Michels <daianaem.12@gmail.com>
2023-04-24 20:55:48 -0300

    start app

---
+++ src/screens/Basket.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { StyleSheet, Image, Dimensions, Text } from "react-native";
+
+import topo from "../../assets/topo.png";
+
+const width = Dimensions.get('screen').width;
+
+export default function Basket() {
+    return (
+    <>
+        <Image source={topo} style={styles.top} />
+        <Text style={styles.title}>Basket Detail</Text>
+    </>
+    ) 
+}
+
+const styles = StyleSheet.create({
+    top: {
+        width: "100%",
+        height: 578 / 768 * width,
+    },
+    title: {
+        width: "100%",
+        position: "absolute",
+        textAlign: "center",
+        fontSize: 16,
+        lineHeight: 26,
+        color: "white",
+        fontWeight: "bold",
+        padding: 16
+    }
+});
