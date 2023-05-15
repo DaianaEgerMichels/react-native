6edaa05bd99b29ce438b09ee72c4985f5e3d6ab6
Daiana Michels <daianaem.12@gmail.com>
2023-05-15 20:33:25 -0300

    list optimization with FlatList

--- src/screens/Basket/index.js
+++ src/screens/Basket/index.js
@@ -1,20 +1,31 @@
 import React from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
+import {FlatList, StyleSheet, View } from "react-native";
 
 import Top from "./components/Top";
 import Details from "./components/Details";
-import Items from "./components/Items";
+import Item from "./components/Item";
+import Texto from "../../components/Texto";
 
 
 export default function Basket({top, details, items}) {
     return (
-    <ScrollView>
-        <Top {...top}/>
-        <View style={styles.basket}>
-            <Details {...details}/>
-            <Items {...items}/>
-        </View>
-    </ScrollView>
+    <>
+        <FlatList
+            data={items.list}
+            renderItem={Item}
+            keyExtractor={({name}) => name}
+            ListHeaderComponent={() => {
+                return <>
+                    <Top {...top}/>
+                    <View style={styles.basket}>
+                        <Details {...details}/>
+                        <Texto style={styles.title}>{items.title}</Texto>
+                    </View>
+                </>
+            }}
+        />
+        
+    </>
     ) 
 }
 
@@ -22,5 +33,13 @@ const styles = StyleSheet.create({
     basket: {
         paddingVertical: 8,
         paddingHorizontal: 16,
-    }  
+    },
+    title: {
+        color: "#464646",
+        fontSize: 20,
+        lineHeight: 32,
+        fontWeight: "bold",
+        marginTop: 32,
+        marginBottom: 8
+    }, 
 });
